7c55e29674ee26f9733c70679e6685703f86196e
Jose Caceres <josecaceres.oreul@gmail.com>
2024-10-08 13:51:35 -0500

    feat: Disable rules for vercel deployment

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,5 +1,6 @@
-export const ENV = "local";
+export const ENV = "vercel";
 
 export const API_URLS = {
   local: 'http://localhost:3000/api',
+  vercel: 'https://calendar-ekmxsw28q-joalcapas-projects.vercel.app/',
 }
